9767d99bc725a29bbd7e8d8356cde93fd03a09f7
Bogdan Lungu <lungu.c.bogdan@gmail.com>
2016-05-30 23:10:56 +0300

    Done client side validation

--- js/script.js
+++ js/script.js
@@ -1,32 +1,41 @@
 $(window).load(function() {
-  $('.send').click(function(e){
-    e.preventDefault();
+    $('.send').click(function(e) {
+        e.preventDefault();
 
-    // setting variables + regex
-    var error = 0;
-    var check_name = /^[A-Za-z0-9 ]{3,20}$/;
-    var check_message = /^[A-Za-z0-9 ]{3,300}$/;
-    var check_email = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+        // setting variables + regex
+        var error = 0;
+        var regexObj = {
+            check_name: /^[A-Za-z0-9 ]{3,20}$/,
+            check_message: /^[A-Za-z0-9,.!?\s]{10,300}$/,
+            check_email: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        }
 
-    var name = $('.name').val();
-    var email = $('.email').val();
-    var message = $('.message').val();
+        var validateObj = {
+            name: $('.name').val(),
+            email: $('.email').val(),
+            message: $('.message').val()
+        }
 
-    var selectToValidate = $('.validate');
-    selectToValidate.each(function(index){
-      console.log($(this).val());
+        var selectToValidate = $('.validate');
+        selectToValidate.each(function(index) {
+            var theInputValue = $(this).val();
+            var typeOfData = $(this).data("input");
+            var propertyName = "check_" + typeOfData;
+
+            if (!regexObj[propertyName].test(validateObj[typeOfData])) {
+                $('.error[data-contact="' + typeOfData + '"]').addClass('show');
+                error = 1;
+            } else {
+                $('.error[data-contact="' + typeOfData + '"]').removeClass('show');
+            }
+        });
+
+        if (error == 0) {
+            console.log("The message has been sent!");
+            $.post("contact.php", function(data, status) {
+                alert("Data: " + data + "\nStatus: " + status);
+            });
+        }
+
     });
-
-    if(!check_name.test(name)){
-       $('.error[data-contact="name"]').addClass('show');
-       error = 1;
-    }else{
-      $('.error[data-contact="name"]').removeClass('show');
-    }
-
-    if(error == 0){
-      console.log("The message has been sent!");
-    }
-
-  });
 });
